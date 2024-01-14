44c44143ceb895142312df1df5c1ead440d968b5
Thando <thandong@webafrica.com>
2024-01-14 19:45:59 +0200

    Add UnitOfWork

---
+++ HotelListing/Data/IUnit/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using HotelListing.Data.IRepository;
+
+namespace HotelListing.Data.IUnit
+{
+    public interface IUnitOfWork : IDisposable 
+    {
+        IGenericRepository<Country> CountriesRepo { get; }
+        IGenericRepository<Hotel> HotelsRepo { get; }
+        Task Save();
+    }
+}
---
+++ HotelListing/Data/Unity/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using HotelListing.Data.Database;
+using HotelListing.Data.IRepository;
+using HotelListing.Data.IUnit;
+using HotelListing.Data.Repository;
+
+namespace HotelListing.Data.Unity
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countriesRepo;
+        private IGenericRepository<Hotel> _hotelsRepo;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Country> CountriesRepo => _countriesRepo ??= new GenericRepository<Country>(_context);
+
+        public IGenericRepository<Hotel> HotelsRepo => _hotelsRepo ??= new GenericRepository<Hotel>(_context);
+
+        // When oparations are done free up memory (gabage collector)
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
